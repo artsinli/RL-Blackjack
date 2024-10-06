1fd2a476862008b3659512c6bc29f80621c42723
artio <artio@DESKTOP-TSN45L2>
2024-10-05 19:05:54 -0700

    Added ace decision tables, finished calculations on the player class. Modified encapsulations of the hand class.

--- Hand.m
+++ Hand.m
@@ -1,21 +1,21 @@
 classdef(Abstract) Hand < handle
     properties 
         current_hand % this is a BlackjackDeck item 
-        hand_value double
+    end
+    properties (Access = protected)
+        hand_value
     end
     methods
         function obj = Hand()
             obj.current_hand = struct('Suit', {}, 'Value', {}, 'BlackjackValue', {});
             obj.hand_value = 0;
         end
-        % function obj = Hand(cards)
-        %     obj.current_hand = cards;
-        %     % MUST CALCULATE THE HAND VALUE AFTER
-        
-        % Cannot discard cards from the players hand, must keep
         function addCard(obj, card)
             obj.current_hand = [obj.current_hand, card];
         end
+    end
+    methods(Abstract, Access = protected)
+        calcHandValue();
     end
 end
 
--- Player.m
+++ Player.m
@@ -1,19 +1,73 @@
 classdef Player < Hand
     % PLAYERHAND The plays and rules associated with a player hand
-    % AActions can be taken here as well. 
-    
-    properties (Access = private)
-        SoftHand 
+    % Actions will be taken here as well
+    properties
+        bet
+        AceTable
+    end
+    % May need to set to public so that a game class may use this 
+    properties (Access = private)  
+        and 
+        nAces
         canDoubleDown 
-        nAces 
+        SoftHand 
     end
     methods
-        function obj = Player()
+        function obj = Player(bet)
+            % Initialize from the super class
+            obj@Hand();
+
+            % Beginning initialization
+            obj.bet = bet;
             obj.SoftHand = false;
             obj.canDoubleDown = false;
+            obj.nAces = 0;
         end
+
         function addCard(obj, card)
+            % Must add cards to play
             addCard@Hand(obj,card);
+
+            % Recalcs the hand value after each draw
+            obj.calcHandValue;
+        end
+    end
+    %% GET FUNCTIONS
+    methods 
+        function result = getHandValue(obj)
+            % Recalculate at each run
+            % obj.calcHandValue % Ensure up to date
+            result = obj.hand_value;
+        end
+    end
+    %% CALCULATION FUNCTIONS
+    methods(Access = protected)
+        function calcAceTable(obj)
+            % Create an ace table, noting the hand_value for each high
+            % chosen ace
+            result = zeros([obj.nAces 2]);
+            
+            % From the first index (0 aces flipped) to nAces (max aces
+            % flipped)
+            for i = 0:obj.nAces
+                result(i+1,:) = [i min(obj.hand_value) + 10*i];
+            end
+            obj.AceTable = result;
+        end
+
+        function calcHandValue(obj)
+            % Reset the hand value to zero to ensure there is no lingering
+            % data or stacking of pluses. 
+            obj.hand_value = 0; 
+            for c = obj.current_hand
+                obj.hand_value = obj.hand_value + c.BlackjackValue;
+                if strcmp(c.Value,'Ace')
+                    obj.nAces = obj.nAces + 1;
+                end
+            end
+            if obj.nAces >= 1
+                obj.calcAceTable();
+            end
         end
     end
 end
