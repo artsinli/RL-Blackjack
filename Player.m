d2b8a460c5345e5c090b772ea436071dd544318c
artio <artio@DESKTOP-TSN45L2>
2024-10-21 21:16:49 -0700

    Completed the initial implementation

--- Player.m
+++ Player.m
@@ -1,70 +1,61 @@
+% PUT ALMOST ALL OF THIS UNDER THE HAND CLASS, bet, softhand and doubledown
+% can stay under player as those aren't dealer related.
+
 classdef Player < Hand
     %% PROPERTIES
     properties
-        bet
-        AceTable
-    end
-    % May need to set to public so that a game class may use this 
-    properties (Access = private)  
-        and 
-        nAces
-        canDoubleDown 
-        SoftHand 
+        playerName
+        MoneyPool
+        canSplit
+        bankrupt
     end
     %% CONSTRUCTORS
     methods
-        function obj = Player(bet)
+        function obj = Player(playerName, playerBank, playerInitalDraw)
+            arguments
+                playerName {mustBeText}
+                playerBank {mustBePositive, mustBeNonzero}
+                playerInitalDraw
+            end
             % Initialize from the super class
-            obj@Hand();
-
-            % Beginning initialization
-            obj.bet = bet;
-            obj.SoftHand = false;
-            obj.canDoubleDown = false;
-            obj.nAces = 0;
+            obj@Hand(playerInitalDraw);
+            obj.MoneyPool = playerBank;
+            obj.playerName = playerName;
+            obj.checkSplitHand;
         end
     end
-    %% PUBLIC FUNCTIONS
-    methods 
-        function result = getHandValue(obj)
-            % Recalculate at each run, 
-            obj.calcHandValue;
-            result = obj.hand_value;
-        end
-
-        function addCard(obj, card)
-            % If no cards are added to player, obj.hand_value is 0
-            addCard@Hand(obj,card);
-            obj.calcHandValue;
+    % methods
+    %     function update(obj)
+    %         obj.checkBankruptcy;
+    %         obj.calcHandValue;
+    %     end
+    % end
+    %% GAME METHODS
+    methods
+        function value = betToPot(obj, bet)
+            obj.checkBankruptcy();
+            if bet >= obj.MoneyPool && ~obj.bankrupt
+                bet = obj.MoneyPool;
+                warning(obj.playerName + ' is now all in. Bet value: ' + bet);
+            end
+            obj.MoneyPool = obj.MoneyPool - bet;
+            value = bet;
         end
     end
-    %% CALCULATION FUNCTIONS
-    methods(Access = protected)
-        function calcAceTable(obj)
-            % Create an ace table, noting the hand_value for each high
-            % chosen ace
-            result = zeros([obj.nAces 2]);
-            
-            % From the first index (0 aces flipped) to nAces (max aces
-            % flipped)
-            for i = 0:obj.nAces
-                result(i+1,:) = [i min(obj.hand_value) + 10*i];
+    methods(Access = private) 
+        function checkBankruptcy(obj)
+            if obj.MoneyPool <= 0 
+                obj.bankrupt = true;
+                disp(obj.playerName + ' is bankrupt.')
+            else
+                obj.bankrupt = false;
             end
-            obj.AceTable = result;
         end
-
-        function calcHandValue(obj)
-            % Resets values for recalc
-            obj.hand_value = 0; 
-            obj.nAces = 0;
-            for c = obj.current_hand
-                obj.hand_value = obj.hand_value + c.BlackjackValue;
-                if strcmp(c.Value,'Ace')
-                    obj.nAces = obj.nAces + 1;
-                end
-            end
-            if obj.nAces >= 1
-                obj.calcAceTable();
+        function checkSplitHand(obj)
+            if isequal(obj.current_hand(1),obj.current_hand(2))
+                obj.canSplit = true;
+            else
+                obj.canSplit = false;
             end
         end
     end
